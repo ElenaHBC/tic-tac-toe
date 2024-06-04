47ee31f586257a6fa430758e3c3d195ac4c191cb
Elena Tomeva <et@healthbiocare.at>
2024-06-04 14:18:37 +0200

    added a comment

--- test_tic-tac-toe.py
+++ test_tic-tac-toe.py
@@ -24,4 +24,9 @@ What to do if third-party code that we use throws exceptions? - I guess we eithe
 d) create, throw and catch exceptions: raise (creates and throws), try, except (catches and handles an error), else, finally (mostly for clean-ups)?
 e) examples of benefits of testing - I guess checking if the code works properly can be done faster and more efficiently;
 also you can "prove' to others that the code works as expected
+
+Since I missed lesson nr.11 and had to catch up on my own, I had some problems with the homework and some unclarity regarding 
+testing and raising/catching errors. I tried to use the ValueError for the tic-tact-toe game in player_move function to catch a not numerical
+user input but it doesn't work and I am not sure how to do this properly.
+Also testing did not work for all of the exercises from the lesson and I need more time to go over everything again. 
 """

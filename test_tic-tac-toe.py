27a8460687090486553283fe4ceb86510bba540b
Elena Tomeva <et@healthbiocare.at>
2024-06-05 10:15:57 +0200

    split the tests into seprate tests functions

--- test_tic-tac-toe.py
+++ test_tic-tac-toe.py
@@ -1,27 +1,34 @@
 from tic_tac_toe_ET import evaluate, move
 
-def test_evaluate():
-    board1 = "-------xxx----------"
-    assert evaluate(board1) == "x"
-    board2 = "ooo-----------------"
-    assert evaluate(board2) == "o"
-    board3 = "xxooxxoxxoxoxoxoxoxo"
-    assert evaluate(board3) == "Draw"
+def test_evaluate_x():
+    board = "-------xxx----------"
+    assert evaluate(board) == "x"
 
-def test_move():
-    board1 = "--------------------"
-    move1 = move(board1, 'x', 1)
-    assert move1 == "-x------------------"
-    board2 = "--------------------"
-    move2 = move(board2, 'o', 19)
-    assert move2 == "-------------------o"
+def test_evaluate_o():
+    board = "ooo-----------------"
+    assert evaluate(board) == "o"
+
+def test_evaluate_draw():
+    board = "xxooxxoxxoxoxoxoxoxo"
+    assert evaluate(board) == "Draw"
+
+def test_move_x():
+    board = "--------------------"
+    move_x = move(board, 'x', 1)
+    assert move_x == "-x------------------"
+
+def test_move_o():
+    board = "--------------------"
+    move_o = move(board, 'o', 19)
+    assert move_o == "-------------------o"
 
 """
 a) python module is an organized block/box of code, while packages contain several modules with sub-modules
 b) side effects of modules are undesirable effects when calling/importing the module, for example if the module contains print statements
 c) exceptions are error messages that have a specific information for the art of error; there are several hierarchial built-in exceptions;
 What to do if third-party code that we use throws exceptions? - I guess we either correct the code or catch the exceptions with except?
-d) create, throw and catch exceptions: raise (creates and throws), try, except (catches and handles an error), else, finally (mostly for clean-ups)?
+d) create, throw and catch exceptions: raise (creates and throws), try, except (catches and handles an error), else, 
+finally (mostly for clean-ups), class to create your own definition of a custom exception based on some built-in exception 
 e) examples of benefits of testing - I guess checking if the code works properly can be done faster and more efficiently;
 also you can "prove' to others that the code works as expected
 
